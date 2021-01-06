b01bc821ac6eac4e87d054bc0cc135a598d3ce20
Dennis <xxyeezusxx@icloud.com>
2021-01-06 15:05:53 +0200

    Initial Commit

---
+++ PROJECT_CITY_MAP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PROJECT_CITY_MAP
+//
+//  Created by Dennis Dimitrov on 6.01.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
